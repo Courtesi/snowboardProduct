b208db3ff6fa010c135aceaadccffc8fa78bcf9d
Courtesi <c.chang673@gmail.com>
2025-01-29 01:15:10 -0800

    ignoring sql_files

--- sql_files/createBankTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE bank (
-  name VARCHAR (255) NOT NULL PRIMARY KEY,
-  balance DECIMAL (19, 2) NOT NULL
-);
-
-INSERT INTO bank(name, balance) VALUES('Peach', 100.00);
-INSERT INTO bank(name, balance) VALUES('Berry', 100.00);
--- sql_files/createUserTable.sql
+++
@@ -1,6 +0,0 @@
--- CREATE TABLE custom_user (
---  username VARCHAR(50) PRIMARY KEY,
---  password VARCHAR(255) NOT NULL
--- );
-
-select * from custom_user;
